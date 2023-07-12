d72ec5cfb304ffe9402c7a6efc9cbb0e7613e39c
User09L <fritzvictorl@gmail.com>
2023-07-13 01:10:12 +0800

    Initial commit

---
+++ Craft&ArtsWebsite/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Craft_ArtsWebsite.Model;
+using Craft_ArtsWebsite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Craft_ArtsWebsite.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
+        public IEnumerable<Product> Products { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger, JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+
+        public void OnGet()
+        {
+            Products = ProductService.GetProducts();
+        }
+    }
+}
---
+++ Craft&ArtsWebsite/Program.cs
@@ -0,0 +1,36 @@
+using Craft_ArtsWebsite.Model;
+using Craft_ArtsWebsite.Services;
+using System.Text.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddTransient<JsonFileProductService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapGet("/products", (context) => {
+    var products = app.Services.GetService<JsonFileProductService>().GetProducts();
+    var Json = JsonSerializer.Serialize<IEnumerable<Product>>(products);
+    return context.Response.WriteAsync(Json);
+
+});
+
+app.Run();
